ea0b056f8c8a2d3c4a6d56fe2f5aed929229dd3a
buntel <julianbantel@gmail.com>
2021-09-23 13:16:31 +0200

    feat(BD-3): add Navbar.vue component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
   <section class="section">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <Navbar/>
   </section>
-  <router-view />
+  <section class="section">
+    <router-view />
+  </section>
 </template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component"
+import Navbar from "@/components/Navbar.vue"
+
+@Options({
+  components: { Navbar },
+})
+export default class App extends Vue {}
+</script>
 
 <style lang="sass">
 @import "./assets/sass/main"
---
+++ src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <router-link to="/" class="navbar-item">Home</router-link>
+
+      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+    <div class="navbar-menu">
+      <router-link to="/about" class="navbar-item">About</router-link>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  props: {
+    msg: String,
+  },
+})
+export default class Navbar extends Vue {
+  msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="sass">
+</style>
