437efbe5d32ebf04bc65add1a295e3cbb181b6dd
buntel <julianbantel@gmail.com>
2021-10-01 13:46:29 +0200

    feat(BD-3): toggle menu on mobile

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,14 +3,14 @@
     <div class="navbar-brand">
       <router-link to="/" class="navbar-item">Home</router-link>
 
-      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" @click="menuOpen = !menuOpen">
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
       </a>
     </div>
-    <div class="navbar-menu">
-      <router-link to="/about" class="navbar-item">About</router-link>
+    <div class="navbar-menu" :class="{open: menuOpen}">
+      <router-link to="/about" class="navbar-item" @click="collapseMenu">About</router-link>
     </div>
   </nav>
 </template>
@@ -25,9 +25,16 @@ import { Options, Vue } from "vue-class-component"
 })
 export default class Navbar extends Vue {
   msg!: string;
+  menuOpen = true
+
+  collapseMenu() {
+    this.menuOpen = false
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="sass">
+  .navbar-menu.open
+    display: block
 </style>
