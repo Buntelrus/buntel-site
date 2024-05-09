1b793633cb09fc5d6ac04d21df1f077c7d0b8c35
buntel <julianbantel@gmail.com>
2024-05-09 21:54:44 +0200

    feat: go under maintenance

--- addblockdetect.js
+++
@@ -1,8 +0,0 @@
-function handleaddblock() {
-    console.log('blocked')
-    fetch('addblock.html')
-        .then(response => response.text())
-        .then(function (responseText) {
-            document.body.innerHTML = responseText;
-        })
-}
